64a738523964e5199cbdc24b16dae469808d9084
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2023-12-21 21:55:15 +0700

    create source code

---
+++ app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
