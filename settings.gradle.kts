6cc59e3288ee2636d16595dd927740f40f800fb1
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2023-12-21 21:56:55 +0700

    create source code

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DoAnAndroid_Nhom10_VanPhongPham"
+include(":app")
+ 
