da36cd395f82a00c4bf4e773bf3314e81ec852a8
Nguyen Van Linh <nguyenvanlinhstudy@gmail.com>
2023-12-22 15:57:56 +0700

    linh update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham"
--- app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/MainActivity.java
+++ app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/MainActivity.java
@@ -1,8 +1,13 @@
 package com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBarMenu();
+    }
 
+    private void ActionBarMenu() {
+        ActionBar actbar = getSupportActionBar();
+        actbar.setTitle("Xuân Cường");
+        actbar.setSubtitle("Văn Phòng Phẩm");
+        actbar.setDisplayHomeAsUpEnabled(true);
+        actbar.setDisplayShowHomeEnabled(true);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        // Check if the selected item is the back button
+        if (id == android.R.id.home) {
+            ExitApp();
+            return true;
+        } else if (id == android.R.id.title) {
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void ExitApp() {
+        AlertDialog.Builder exit = new AlertDialog.Builder(this);
+        exit.setTitle("Thông Báo")
+                .setMessage("Bạn có muốn thoát ứng dụng không?")
+                .setCancelable(false)
+                .setPositiveButton("Có", (dialog, which) -> {
+                    finish();
+                })
+                .setNegativeButton("Không", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+        exit.create().show();
     }
 }
