2f50316a107a7bc96dcaf831a12aeec0ee79e2cd
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2023-12-23 00:57:06 +0700

    update

--- app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/MainActivity.java
+++ app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/MainActivity.java
@@ -21,9 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        AnhXa();
     }
-
 
 
     private void AnhXa() {
