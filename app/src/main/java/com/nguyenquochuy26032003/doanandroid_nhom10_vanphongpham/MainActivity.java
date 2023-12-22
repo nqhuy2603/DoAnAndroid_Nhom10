081190ba835573076f660c7f3ac99d329db3bfd4
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2023-12-22 18:07:15 +0700

    create Trang Chu

--- app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/MainActivity.java
+++ app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/MainActivity.java
@@ -1,16 +1,36 @@
 package com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewTrangChu;
+    NavigationView navigationView;
+    ListView listViewTrangChu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
+
+
+    private void AnhXa() {
+        toolbar = findViewById(R.id.toolBarTrangChu);
+        viewFlipper = findViewById(R.id.viewFlipper);
+        recyclerViewTrangChu = findViewById(R.id.recyclerview);
+        navigationView = findViewById(R.id.navigationView);
+        listViewTrangChu= findViewById(R.id.listViewTrangChu);
     }
 }
