ad9a1583396f0bde18c67559dd548aadfc925aea
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2023-12-27 11:14:44 +0700

    update

--- app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/activity/SachActivity.java
+++ app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/activity/SachActivity.java
@@ -1,18 +1,41 @@
 package com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham.R;
+import com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham.adapter.SachAdapter;
+import com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham.data.DatabaseManager;
 
 public class SachActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
 
+    SachAdapter sachAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sach);
 
-
+        AnhXa();
+        ActionToolBar();
     }
+
+    private void ActionToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void AnhXa() {
+        toolbar = findViewById(R.id.toolbarSach);
+        recyclerView = findViewById(R.id.recyclerviewSach);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+    }
+
 }
--- app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/adapter/SachAdapter.java
+++ app/src/main/java/com/nguyenquochuy26032003/doanandroid_nhom10_vanphongpham/adapter/SachAdapter.java
@@ -1,21 +1,62 @@
 package com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham.R;
 import com.nguyenquochuy26032003.doanandroid_nhom10_vanphongpham.doituong.Item;
 
 import java.util.List;
 
-//public class SachAdapter extends RecyclerView.Adapter<SachAdapter.MyViewHolder> {
-//    Context context;
-//    List<Item> array;
-//    public class MyViewHolder extends RecyclerView.ViewHolder {
-//        public MyViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//        }
-//    }
-//}
+public class SachAdapter extends RecyclerView.Adapter<SachAdapter.MyViewHolder> {
+    Context context;
+    List<Item> array;
+
+    public SachAdapter(Context context, List<Item> array) {
+        this.context = context;
+        this.array = array;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_sach, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Item item = array.get(position);
+        holder.nameItem.setText(item.getName());
+        double price = item.getPrice();
+        String formattedPrice = String.format("%,.0f đ", price);
+        holder.priceItem.setText(formattedPrice);
+        Glide.with(context).load(item.getImage()).into(holder.imageItem);
+        holder.describeItem.setText(item.getDescribe());
+    }
+
+    @Override
+    public int getItemCount() {
+        return array.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView nameItem, priceItem, describeItem;
+        ImageView imageItem;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameItem = itemView.findViewById(R.id.textviewNameItemSach);
+            priceItem = itemView.findViewById(R.id.textviewPriceItemSach);
+            describeItem = itemView.findViewById(R.id.textviewDescribeItemSach);
+            imageItem = itemView.findViewById(R.id.imageItemSach);
+        }
+    }
+}
